a6e7ad61bd3bc9c343b51efc3c0bc3f960b48990
47StanyS <stanyslavalokhin@gmail.com>
2024-03-20 21:11:05 +0200

    Elements

--- Elements-1/Elements-1/Elements-1/Program.cs
+++ Elements-1/Elements-1/Elements-1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Elements
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] ints = new int[50];
+            var counter = 0;
+
+            foreach (int value in ints)
+            {
+                counter += 2;
+                ints[value] = counter;
+                Console.WriteLine($"Elements: {ints[value]}");
+            }
+        }
+    }
+}
--- Elements-1/Elements-1/Elements-1/obj/Debug/net8.0/Elements-1.AssemblyInfo.cs
+++ Elements-1/Elements-1/Elements-1/obj/Debug/net8.0/Elements-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Elements-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e960ebfd3448d2b24a31832ff9a5eb50daaeed8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Elements-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Elements-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
