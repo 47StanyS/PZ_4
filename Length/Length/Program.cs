ea6e02be880d66f1fb8c0ab105bd1f393298df81
47StanyS <stanyslavalokhin@gmail.com>
2024-03-20 20:29:34 +0200

    Symba

--- Length/Length/Program.cs
+++ Length/Length/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Length
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write text: ");
+
+            var writeText = Console.ReadLine();
+
+            Console.WriteLine($"Text: '{writeText}' include:'{writeText.Length}' symbols.");
+        }
+    }
+}
--- Length/Length/obj/Debug/net8.0/Length.AssemblyInfo.cs
+++ Length/Length/obj/Debug/net8.0/Length.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Length")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63bca6b44a00d99264b78eaf1d762f690f54ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("Length")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Length")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
