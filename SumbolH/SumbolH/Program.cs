1bf292bfb0f444f085146b984c70f3e18fe34cf2
47StanyS <stanyslavalokhin@gmail.com>
2024-03-20 20:22:51 +0200

    Sym

--- SumbolH/SumbolH/Program.cs
+++ SumbolH/SumbolH/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SumbolH
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("   ______");
+            Console.WriteLine("  /Write/");
+            Console.WriteLine(" /text!/ ");
+            Console.WriteLine(" ------");
+            //Console.WriteLine($"Write text: ");
+
+            var writeText = Console.ReadLine();
+
+            Console.WriteLine("  _  ");
+            Console.WriteLine(" | |  Choose");
+            Console.WriteLine("_| |_  a");
+            Console.WriteLine("\\   / letter");
+            Console.WriteLine(" \\ /  ");
+            Console.WriteLine("  -  ");
+            //Console.WriteLine("choose a letter");
+
+            char chooseSym = Console.ReadLine()[0];
+
+            Console.WriteLine("_________________\n");
+
+            int lenght = writeText.Length;
+            int count = 0;
+
+            foreach (char ch in writeText.ToLower())
+            {
+                if (ch == char.ToLower(chooseSym))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Text: '{writeText}' include: {writeText.Length} symbols.");
+            Console.WriteLine($"Number of characters found '{chooseSym}': {count}");
+
+        }
+    }
+}
--- SumbolH/SumbolH/obj/Debug/net8.0/SumbolH.AssemblyInfo.cs
+++ SumbolH/SumbolH/obj/Debug/net8.0/SumbolH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SumbolH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d51765ad4478a502f73ea6c1f4970a4f56f088")]
 [assembly: System.Reflection.AssemblyProductAttribute("SumbolH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SumbolH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
